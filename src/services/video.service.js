3dc707816425b3f051ce9cc0c1d5f6c84f67cc9e
HarshSharma018 <harshjangid033@gmail.com>
2026-08-16 23:21:59 +0530

    function logic changes

--- src/services/video.service.js
+++ src/services/video.service.js
@@ -1,12 +1,12 @@
 const { exec } = require('child_process');
+const path = require('path');
 
 function getVideoInfo(url) {
   return new Promise((resolve, reject) => {
+    const command = `yt-dlp -j "${url}"`;
 
-    exec(`yt-dlp -j "${url}"`, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
-      if (error) {
-        return reject(error);
-      }
+    exec(command, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
+      if (error) return reject(error);
 
       const data = JSON.parse(stdout);
 
@@ -22,6 +22,22 @@ function getVideoInfo(url) {
           url: f.url,
         })),
       });
+    });
+  });
+}
+
+function downloadVideo(url) {
+  return new Promise((resolve, reject) => {
+    const outputTemplate = path.join(__dirname, '../../downloads/%(id)s.%(ext)s');
+    const command = `yt-dlp -f "best[ext=mp4]" -o "${outputTemplate}" --print filename "${url}"`;
+
+    exec(command, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
+      if (error) return reject(error);
+
+      const filePath = stdout.trim();
+      const fileName = path.basename(filePath);
+
+      resolve({ filePath, fileName });
     });
   });
 }
