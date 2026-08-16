07e07f37cba6e15ebc7db5a3ff696dee8a8e237e
HarshSharma018 <harshjangid033@gmail.com>
2026-08-16 23:18:51 +0530

    add jobroutes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const downloadRoutes = require('./routes/download.routes');
 
 const app = express();
+const path = require('path');
+const jobRoutes = require('./routes/job.routes');
+
+app.use('/api/jobs', jobRoutes);
+app.use('/files', express.static(path.join(__dirname, '../downloads')));
 
 app.use(express.json());
 
